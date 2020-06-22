f04228d9f1c71d102e97d30129fdb04bd575ff74
Yogesh Khichi <yogeshkkhichi@gmail.com>
2020-06-22 07:05:33 +0530

    add using splice and copy using slice

--- BasicDataStuct.js
+++ BasicDataStuct.js
@@ -13,3 +13,28 @@
 // array.splice(2, 2);
 // // remove 2 elements beginning with the 3rd element
 // // array now equals ['today', 'was', 'great']
+
+
+
+
+// // ADD ITEMS USING THE SPLICE METHOD
+// const numbers = [10, 11, 12, 12, 15];
+// const startIndex = 3;
+// const amountToDelete = 1;
+
+// numbers.splice(startIndex, amountToDelete, 13, 14);
+// // the second entry of 12 is removed, and we add 13 and 14 at the same index
+// console.log(numbers);
+// // returns [ 10, 11, 12, 13, 14, 15 ]
+
+
+
+
+
+// // COPY ARRAY ELEMENTS USING THE SLICE METHOD
+// let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+
+// let todaysWeather = weatherConditions.slice(1, 3);
+// // todaysWeather equals ['snow', 'sleet'];
+// // weatherConditions still equals ['rain', 'snow', 'sleet', 'hail', 'clear']
+
