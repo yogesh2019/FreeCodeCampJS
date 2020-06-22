4ef2a20214851565902fbae86f04686f09b6ef24
Yogesh Khichi <yogeshkkhichi@gmail.com>
2020-06-22 07:19:38 +0530

    complex arrays

--- BasicDataStuct.js
+++ BasicDataStuct.js
@@ -93,4 +93,25 @@
 
 
 
+// // create complex array
+// let nestedArray = [ // top, or first level - the outer most array
+//     ['deep'], // an array within an array, 2 levels of depth
+//     [
+//       ['deeper'], ['deeper'] // 2 arrays nested 3 levels deep
+//     ],
+//     [
+//       [
+//         ['deepest'], ['deepest'] // 2 arrays nested 4 levels deep
+//       ],
+//       [
+//         [
+//           ['deepest-est?'] // an array nested 5 levels deep
+//         ]
+//       ]
+//     ]
+//   ];
+  
+
+
+
 
