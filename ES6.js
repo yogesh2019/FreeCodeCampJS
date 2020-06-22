cba7a28f54ce8d958df3156d2cc72c7a0703213b
Yogesh Khichi <yogeshkkhichi@gmail.com>
2020-06-22 06:29:16 +0530

    compare scope of let and var

--- ES6.js
+++ ES6.js
@@ -6,5 +6,42 @@
 
 // so by using the 'let' a variable with same name can only be declared once
 // Note the "use strict". This enables Strict Mode, which catches common coding mistakes and "unsafe" actions. For instance:
-"use strict";
-x = 3.17;
+// "use strict";
+// x = 3.17;
+
+
+
+
+
+
+
+
+
+// scope of var and let keyword
+// when you declare let keyword inside a block, statement or expression its scope is limited to that block, statement or expression
+// var printNumTwo;
+// for (var i = 0; i < 3; i++) {
+//   if (i === 2) {
+//     printNumTwo = function() {
+//       return i;
+//     };
+//   }
+// }
+// console.log(printNumTwo());
+// // returns 3
+
+
+// 'use strict';
+// let printNumTwo;
+// for (let i = 0; i < 3; i++) {
+//   if (i === 2) {
+//     printNumTwo = function() {
+//       return i;
+//     };
+//   }
+// }
+// console.log(printNumTwo());
+// // returns 2
+// console.log(i);
+// // returns "i is not defined"
+
