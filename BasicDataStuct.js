8d42d9a095ccad1ef004dacc0a5960433b1ab145
Yogesh Khichi <yogeshkkhichi@gmail.com>
2020-06-22 06:59:13 +0530

    shift pop unshift push remove use of splice

--- BasicDataStuct.js
+++ BasicDataStuct.js
@@ -3,3 +3,13 @@
 
 // access the content using the bracket notation
 
+// add using unshift() and push() 
+// remove using shift() and pop()
+
+// Remove items using splice()
+
+// let array = ['today', 'was', 'not', 'so', 'great'];
+
+// array.splice(2, 2);
+// // remove 2 elements beginning with the 3rd element
+// // array now equals ['today', 'was', 'great']
